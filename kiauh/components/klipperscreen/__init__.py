c5c19066567141448296c117d13825306a0171ec
Vlad <427departament@gmail.com>
2025-12-14 13:58:32 +0300

    Исправлние ветки

--- kiauh/components/klipperscreen/__init__.py
+++ kiauh/components/klipperscreen/__init__.py
@@ -12,7 +12,7 @@ from core.constants import SYSTEMD
 
 # repo
 KLIPPERSCREEN_REPO = "https://github.com/Z-Bolt/KlipperScreen.git"
-KLIPPERSCREEN_BRANCH = "Z-BotlUI3.1"
+KLIPPERSCREEN_BRANCH = "Z-BoltUI3.1"
 
 # names
 KLIPPERSCREEN_SERVICE_NAME = "KlipperScreen.service"
